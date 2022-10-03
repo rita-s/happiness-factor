c070739ab0ba900b946df467c6767cd6578baabe
rita-s <rita_s@mail.com>
2022-10-03 17:54:52 +0100

    adding WORKING slider! winner winner chicken dinner!

---
+++ static/scripts.js
@@ -0,0 +1,22 @@
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = '../static/images/family.jpg';
+images[1] = '../static/images/lonfon_background.jpg';
+images[2] = '../static/images/education.jpg';
+images[3] = '../static/images/woman_calculating.jpg';
+images[4] = '../static/images/home.jpg';
+
+function changeImg(){
+    document.slide.src = images[i];
+
+    if(i < images.length - 1){
+    i++;
+} else {
+    i = 0;
+}
+setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
